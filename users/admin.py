8657c926ce8a2a88a3aaafb66da2ad655506fc45
Kennedy <121739110+kbrieva@users.noreply.github.com>
2024-03-06 16:35:31 +0800

    Latest code with images

--- users/admin.py
+++ users/admin.py
@@ -6,6 +6,6 @@ admin.site.site_header = "Profiler Admin"
 admin.site.site_title = "ProfilerApp admin area"
 admin.site.index_title = "Welcome to Profiler App!"
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['user_fname', 'user_lname', 'user_email', 'user_position']
+    list_display = ['image_tag', 'user_fname', 'user_lname', 'user_email', 'user_position']
     search_fields = ['user_fname', 'user_lname', 'user_email', 'user_position']
 admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -9,11 +9,10 @@ now = timezone.now()
 def image_path(instance, filename):
     basefilename, file_extension = os.path.splitext(filename)
     chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-    randomstr  =''.join((random.choice(chars)))
+    randomstr = ''.join((random.choice(chars)))
     _now = datetime.now()
 
-    return 'profile_pic/{year}-{month}-{imageid}-{randomstr}{ext}'.format(
-        imageid= instance, basename=basefilename, randomstring=randomstr, ext=file_extension, year=_now.strftime('%Y'), month=_now.strftime('%m'), day=_now.strftime('%d') )
+    return 'profile_pic/{year}-{month}-{imageid}-{basename}-{randomstring}{ext}'.format(imageid= instance, basename=basefilename, randomstring=randomstr, ext=file_extension, year=_now.strftime('%Y'), month=_now.strftime('%m'), day=_now.strftime('%d'))
 class User(models.Model):
     user_fname = models.CharField(max_length=50, verbose_name='First Name')
     user_lname = models.CharField(max_length=50, verbose_name='Last Name')
@@ -22,7 +21,7 @@ class User(models.Model):
     user_image = models.ImageField(upload_to=image_path, default='profile_pic/image.jpg')
     
     def image_tag(self):
-        return mark_safe('<img scr="/users/media/%s" width="50" height="50" />' % self.user_image)
+        return mark_safe('<img src="/users/media/%s" width="50" height="50" />'%(self.user_image))
 
     
     
